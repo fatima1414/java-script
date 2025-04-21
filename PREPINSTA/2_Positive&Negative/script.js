4a8e2bc45d629e391fd7d0b5a977aabe1e4a7bc0
fatima1414 <shaikhfatima5260@gmai.com>
2025-04-21 14:07:11 +0530

    code

---
+++ PREPINSTA/2_Positive&Negative/script.js
@@ -0,0 +1,34 @@
+
+// let  result = document.getElementById("answer");
+// function checkData(){
+//     let inputNum =parseInt(document.getElementById("input-num").value);
+//     if (inputNum >= 0) {
+//         result.innerHTML = "Positive Number!";  
+//   } else {
+//        result.innerHTML= "Negative Number!";  
+//   }
+// }
+
+// let  result = document.getElementById("answer");
+// function checkData(){
+//     let inputNum =parseInt(document.getElementById("input-num").value);
+//     if (inputNum >= 0) {
+//         result.innerText = "Positive Number!";  
+//   } else {
+//        result.innerText= "Negative Number!";  
+//   }
+// }
+
+
+
+// SIR CODE
+let result = document.getElementById("answer");
+function checkData() {
+    let inputNum = parseInt(document.getElementById("input-num").value);
+
+    if (inputNum >= 0) {
+          result.innerText = "Positive Number!";  
+    } else {
+         result.innerText = "Negative Number!";  
+    }
+}
