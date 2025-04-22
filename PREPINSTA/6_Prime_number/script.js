86751039ed5cc798fceb628c3de0cf08d7300e9b
fatima1414 <shaikhfatima5260@gmai.com>
2025-04-22 13:26:48 +0530

    code

---
+++ PREPINSTA/6_Prime_number/script.js
@@ -0,0 +1,81 @@
+// let print = document.getElementById("prime");
+
+// function checkData() {
+//     let num1 = parseInt(document.getElementById("input-num1").value);
+//     let print = document.getElementById("prime");
+//     let count = 0;
+
+
+//prime number
+//  let num1=15;
+//  let count = 0;
+
+// for(let i=1;  i <= num1; i++){
+//     if(num1%i==0){
+//         ++count;
+//     }
+// }
+
+// if(count ==2){
+//     console.log("prime number")
+// }else{
+//     console.log("not prime number")
+// }
+
+// prime number with range
+// let start = 50;
+// let end = 100;
+// for (let num = start; num <= end; num++) {
+//     let count = 0;
+//     for (let i = 1; i <= num; i++) {
+       
+//         if (num % i == 0) {
+//             ++count;
+         
+//         }
+//     }
+
+//     if (count == 2) {
+//         console.log(num)
+//     }
+// }
+
+//PRIME NUMBER URL
+function checkData() {
+    let start= parseInt(document.getElementById("input-num1").value);
+    // let end  = parseInt(document.getElementById("input-num1").value);
+    let print = document.getElementById("prime");
+
+    for (let num = 1; num <= start; num++) {
+            let count = 0;
+            for (let i = 1; i <= num; i++) {
+               
+                if (num % i == 0) {
+                    ++count;
+                 
+                }
+            }
+        
+            if (count == 2) {
+                print.innerText = "it is a prime number";
+            }else{
+                print.innerText = "it is a not prime number";
+            }
+        }
+    }
+
+
+// for (let num = start; num <= end; num++) {
+//     let count = 0;
+//     for (let i = 1; i <= num; i++) {
+//         if (num % i == 0) {
+//             count++;
+//         }
+//     }
+//     if (count == 2) {
+//         result += num + " ";
+//     }
+// }
+
+// print.innerText = "Prime numbers: " + result;
+// }
---
+++ PREPINSTA/6_Prime_number/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+// function checkData() {
+//     let num1 = parseInt(document.getElementById("input-num1").value);
+//     let print = document.getElementById("prime");
+//     let count = 0;
